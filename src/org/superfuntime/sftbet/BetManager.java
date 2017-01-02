0346219aae9c49c748a833c70152082ad4c1f447
KosmicCo <cruzbarnum@gmail.com>
2017-01-01 22:03:11 -0800

    Started the /bet command.

--- src/org/superfuntime/sftbet/BetManager.java
+++ src/org/superfuntime/sftbet/BetManager.java
@@ -9,10 +9,11 @@ import java.util.*;
 public class BetManager {
     // use decimals for %
     private static double winnerPercent = 0.05; // auto percentage of pot that winner fighter takes
-
+    private int state; // 0-Betting 1-fighting >1-done
     private Map<String, Map<String, Double>> betInfo; // all the other people
 
     public BetManager(Collection<String> fighters){
+        state = 0;
         betInfo = new HashMap();
         for(String ftr : fighters){
             betInfo.put(ftr, new HashMap()); // init people to bet for
@@ -21,7 +22,7 @@ public class BetManager {
 
     public boolean addBet(String fighter, String name, double amount){
         Map bets = betInfo.get(fighter);
-        if(bets != null){
+        if(bets != null && state == 0){
             if(bets.containsKey(name)) {
                 bets.replace(name, amount);
             }else{
@@ -38,52 +39,60 @@ public class BetManager {
 
     public static double getWinnerPercent(){ return winnerPercent; }
 
+    public void setState(int s){ state = s; }
+
+    public int getState(){ return state; }
+
     public Set<String> getFighters(){ return betInfo.keySet();}
 
     public Map<String, Double> calcWins(String winner){
-        // first part gathers all the money from the losers
-        double loseAmount = 0.0;
-        for(String loser : betInfo.keySet()){
-            if(!loser.equals(winner)){
-                for(Double money : betInfo.get(loser).values()){
-                    loseAmount += money;
+        if(state >= 2) {
+            // first part gathers all the money from the losers
+            double loseAmount = 0.0;
+            for (String loser : betInfo.keySet()) {
+                if (!loser.equals(winner)) {
+                    for (Double money : betInfo.get(loser).values()) {
+                        loseAmount += money;
+                    }
                 }
             }
-        }
 
-        // second part distributes money based on bet amount (plus default fighter win amount)
-        double wins = loseAmount * winnerPercent; // money that fighter takes
-        loseAmount -= wins;
+            // second part distributes money based on bet amount (plus default fighter win amount)
+            double wins = loseAmount * winnerPercent; // money that fighter takes
+            loseAmount -= wins;
 
-        double winBet = 0.0;
-        for(Double money : betInfo.get(winner).values()) {
-            winBet += money;
-        }
+            double winBet = 0.0;
+            for (Double money : betInfo.get(winner).values()) {
+                winBet += money;
+            }
 
-        Map<String, Double> gains = new HashMap();
+            Map<String, Double> gains = new HashMap();
 
-        // calculates the percent of pot that goes to each bet winner
-        if(winBet != 0) {
-            for (String wnr : betInfo.get(winner).keySet()) {
-                int mg = ((int) (100 * (loseAmount * (betInfo.get(winner).get(wnr) / winBet))));
-                if(mg != 0) {
-                    gains.put(wnr, mg * 0.01);
+            // calculates the percent of pot that goes to each bet winner
+            if (winBet != 0) {
+                for (String wnr : betInfo.get(winner).keySet()) {
+                    int mg = ((int) (100 * (loseAmount * (betInfo.get(winner).get(wnr) / winBet))));
+                    if (mg != 0) {
+                        gains.put(wnr, mg * 0.01);
+                    }
                 }
             }
+
+            // checks to see whether all the money should go to fighter due to lack of bet winners (other than fighter)
+            if (gains.isEmpty()) {
+                gains.put(winner, loseAmount);
+            }
+
+            if (gains.containsKey(winner)) { // makes sure to add fighter's winnings to total
+                gains.replace(winner, ((int) (100 * (gains.get(winner) + wins))) * 0.01);
+            } else {
+                gains.put(winner, ((int) (100 * wins)) * 0.01);
+            }
+
+            return gains;
         }
 
-        // checks to see whether all the money should go to fighter due to lack of bet winners (other than fighter)
-        if(gains.isEmpty()){
-            gains.put(winner, loseAmount);
-        }
-
-        if(gains.containsKey(winner)){ // makes sure to add fighter's winnings to total
-            gains.replace(winner, ((int) (100 * (gains.get(winner) + wins))) * 0.01);
-        }else{
-            gains.put(winner, ((int) (100 * wins)) * 0.01);
-        }
-
-        return gains;
+        return null;
     }
 
     @Override
--- src/org/superfuntime/sftbet/SFTBet.java
+++ src/org/superfuntime/sftbet/SFTBet.java
@@ -219,16 +219,41 @@ public class SFTBet extends JavaPlugin implements Listener {
         } else if (cmd.getName().equalsIgnoreCase("bet") && !betDisabled) {
 
             if (sender instanceof Player) {
+                Player player = (Player) sender;
+                boolean first = true;
+                try {
+                    if (args.length >= 3) {
+                        int matchid = Integer.parseInt(args[0]);
+                        first = false;
+                        if (queue.size() > matchid && matchid >= 0) {
+                            BetManager match = queue.get(matchid);
+                            boolean worked = false;
+                            if (match != null) {
+                                worked = match.addBet(args[1], player.getName(), Integer.parseInt(args[2]));
+                            }
+                            if(!worked){
+                                if(match.getState() != 0){
+                                    sender.sendMessage(ChatColor.YELLOW + "Betting for match " + matchid + " has ended already.");
+                                }else{
+                                    sender.sendMessage(ChatColor.YELLOW + "There is no fighter " + args[1] + " in match " + matchid);
+                                }
+                            }
+                        } else {
 
-                if (args.length > 0) {
+                            sender.sendMessage(ChatColor.YELLOW + "Use '/bet help' to show usages.");
+                        }
 
-                } else {
+                    } else {
 
-                    sender.sendMessage(ChatColor.YELLOW + "Use '/bet help' to show usages.");
+                        sender.sendMessage(ChatColor.DARK_RED + "Non-player senders may not bet on matches.");
+                    }
+                } catch (Exception e) {
+                    if (first) {
+                        sender.sendMessage(ChatColor.RED + args[0] + " must be an valid match number.");
+                    } else {
+                        sender.sendMessage(ChatColor.RED + args[2] + " must be an valid amount of money.");
+                    }
                 }
-            } else {
-
-                sender.sendMessage(ChatColor.DARK_RED + "Non-player senders may not bet on matches.");
             }
         } else if (cmd.getName().equalsIgnoreCase("disable")) {
 
